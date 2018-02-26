3141c32ee07c4963e53a358913980bc49f5acfe6
Tet Contado <delrio.tet@gmail.com>
2018-02-25 23:05:33 -0800

    removed commented lines

--- final/movie/dbconnection.php
+++ final/movie/dbconnection.php
@@ -1,15 +1,4 @@
 <?php
-//$dsn = 'mysql:host=localhost;dbname=moviedb';
-//$username = 'root';
-//$password = 'root';
-//
-//try {
-//    $db = new PDO($dsn, $username, $password);
-//} catch (PDOException $e) {
-//    $error_message = $e->getMessage();
-//    include('database_error.php');
-//    exit();
-//}
 
 class Database {
     private static $dsn = 'mysql:host=localhost;dbname=moviedb';
